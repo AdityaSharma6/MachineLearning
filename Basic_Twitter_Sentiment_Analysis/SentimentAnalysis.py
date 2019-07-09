86972ded4cf2cf42e1d93debfe84f45a66a8d020
Aditya Sharma <adityasharma@Adityas-MacBook-Pro.local>
2019-07-08 22:55:25 -0400

    Followed OOP Convention

--- Basic_Twitter_Sentiment_Analysis/SentimentAnalysis.py
+++ Basic_Twitter_Sentiment_Analysis/SentimentAnalysis.py
@@ -1,9 +1,10 @@
 # API is a gate way that lets users access a server's internal functionality
 from textblob import TextBlob
+import matplotlib as plt 
 import tweepy
 import json
 
-<<<<<<< HEAD
+
 class SentimentAnalysis():
     def __init__(self):
         self.consumer_key = "kQo51pA3BgWEuBnF7I1srqFfM"
@@ -11,13 +12,15 @@ class SentimentAnalysis():
         self.access_token = "1147611631448449027-FNo5Muxaa091HOWuKz2fXNXQPEYJkh"
         self.access_token_secret = "kKJVeI9Wgt1XdnwHYrbsYc2aI4Xt0up2t1WuORrr0HcoX"
     
+
     def setupTwitter(self):
         auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
         auth.set_access_token(self.access_token, self.access_token_secret)
         self.api = tweepy.API(auth)
     
+
     def findFollowers(self):
-        following = self.api.followers("@Kajanth_K", count=200000)
+        following = self.api.followers("@Kajanth_K", count=200)
         
         for i in range(len(following)):
             print(i)
@@ -25,54 +28,41 @@ class SentimentAnalysis():
             print(following[i]._json["screen_name"])
             print("\n")
         print(len(following))
+
+    
+    def searchUser(self):
+        self.publicTweets = self.api.search("Trump")
+        self.sentiment = []
+        self.count = {"negative": 0, "positive": 0}
+
+        for i in self.publicTweets:
+            print(i.text)
+            analysis = TextBlob(i.text)
+            value = analysis.sentiment.polarity
+            self.sentiment.append(value)
+    
+            if (value) > 0:
+                self.count["positive"] += 1
+            elif (value) < 0:
+                self.count["negative"] += 1
+            else:
+                continue
+
+
+    def returnResults(self):
+        average_perspective = sum(self.sentiment) / len(self.sentiment)
+        frequent_perspective = self.count
+
+        print(average_perspective)
+        print(frequent_perspective)
+
     
     def execute(self):
         self.setupTwitter()
-        self.findFollowers()
+        self.searchUser()
+        self.returnResults()
+
 
 if __name__ == "__main__":
     trial = SentimentAnalysis()
     trial.execute()
-
-'''
-=======
-consumer_key = "KEY"
-consumer_secret = "KEY"
-access_token = "TOKEN"
-access_token_secret = "TOKEN"
->>>>>>> 2cda0b6336f9e5be931fddafbcab3c0cf4457cd5
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-API = tweepy.API(auth)
-
-publicTweets = API.search("Kamal Khera Lib")
-sentiment = []
-count = {"negative": 0, "positive": 0}
-
-for i in publicTweets:
-    print(i.text)
-    analysis = TextBlob(i.text)
-    value = analysis.sentiment.polarity
-    sentiment.append(value)
-    
-    if (value) > 0:
-        count["positive"] += 1
-    elif (value) < 0:
-        count["negative"] += 1
-    else:
-        continue
-        
-
-average_perspective = sum(sentiment) / len(sentiment)
-frequent_perspective = count
-
-print(average_perspective)
-print(frequent_perspective)
-<<<<<<< HEAD
-'''
-=======
-
-    
->>>>>>> 2cda0b6336f9e5be931fddafbcab3c0cf4457cd5
