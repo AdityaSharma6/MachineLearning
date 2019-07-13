d075859a44304ed8bcf428e3cb44c2f269fce413
Aditya Sharma <22263756+AdityaSharma6@users.noreply.github.com>
2019-07-12 21:58:43 -0400

    Set variables to make it easy to change parameters

--- Basic_Twitter_Sentiment_Analysis/SentimentAnalysis.py
+++ Basic_Twitter_Sentiment_Analysis/SentimentAnalysis.py
@@ -10,39 +10,49 @@ import csv
 
 class SentimentAnalysis():
 
+    def __init__(self):
+        self.CONSUMER_KEY = Authorize.CONSUMER_KEY
+        self.CONSUMER_SECRET = Authorize.CONSUMER_SECRET
+        self.ACCESS_TOKEN = Authorize.ACCESS_TOKEN
+        self.ACCESS_TOKEN_SECRET = Authorize.ACCESS_TOKEN_SECRET
+
+        self.past_date = datetime.date(2019, 7, 5)
+        self.next_day = datetime.date(2019, 7, 6)
+        self.future_date = datetime.date(2019, 7, 10)
+
+        self.tweets_list = []
+
+        self.query = "Donald Trump"
+        self.daily_tweet_count = 100
+
     def setupTwitter(self):
-        auth = tweepy.OAuthHandler(Authorize.CONSUMER_KEY, Authorize.CONSUMER_SECRET)
-        auth.set_access_token(Authorize.ACCESS_TOKEN, Authorize.ACCESS_TOKEN_SECRET)
+        auth = tweepy.OAuthHandler(self.CONSUMER_KEY, self.CONSUMER_SECRET)
+        auth.set_access_token(self.ACCESS_TOKEN, self.ACCESS_TOKEN_SECRET)
         self.api = tweepy.API(auth)
     
     def getUser(self):
-        userObject = self.api.get_user("@realDonaldTrump")
-        #print(userObject)
+        user_object = self.api.get_user("@realDonaldTrump")
+        print(user_object)
     
-    def dateTime(self):
-        pastDate = datetime.date(2019, 7, 5)
-        untilDate = datetime.date(2019, 7, 6)
-        currentDate = datetime.date(2019, 7, 10)
-        difference = currentDate - pastDate
-        print(difference)
-        self.tweetsList = []
-
-        for i in range(difference.days):
-            pastDate += datetime.timedelta(days = 1)
-            untilDate += datetime.timedelta(days = 1)
-            tweets = tweepy.Cursor(self.api.search, q = "Donald Trump", since = pastDate, until = untilDate).items(100) # You can search for hashtags by replacing the Query with your designated Hashtag
-            print("############################# \n")
-            k = 0
+    def time_difference(self):
+        self.difference = (self.future_date - self.past_date).days
+    
+    def extractTweets(self):
+        for i in range(self.difference):
+            self.past_date += datetime.timedelta(days = 1)
+            self.next_day += datetime.timedelta(days = 1)
+            tweets = tweepy.Cursor(self.api.search, q = self.query, since = self.past_date, until = self.next_day, lang = "en").items(self.daily_tweet_count) # You can search for hashtags by replacing the Query with your designated Hashtag
+            tweet_counter = 0
             for j in tweets:
-                print(k)
-                k += 1
-                print(pastDate)
-                self.tweetsList.append(j.text)
+                print(tweet_counter)
+                tweet_counter += 1
+                print(self.past_date)
+                self.tweets_list.append([j.text, self.past_date])
     
     def writeCSV(self):
         with open('Tweets.csv', 'w') as csvFile:
             writer = csv.writer(csvFile)
-            writer.writerows(self.tweetsList)
+            writer.writerows(self.tweets_list)
 
             
         '''
@@ -85,7 +95,7 @@ class SentimentAnalysis():
     def execute(self):
         self.setupTwitter()
         self.getUser()
-        self.dateTime()
+        self.time_difference()
         self.writeCSV()
         #self.searchUser()
         #self.returnResults()
