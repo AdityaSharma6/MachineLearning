60d6fcaa6a3e0634f8d63f9cedf0eb2f7ff2ca00
Aditya Sharma <22263756+AdityaSharma6@users.noreply.github.com>
2019-08-06 17:05:11 -0400

    Fixed visualization. Subplots made.

--- Linear_Regression/Project_1/Linear_Regression.py
+++ Linear_Regression/Project_1/Linear_Regression.py
@@ -1,20 +1,21 @@
 import pandas as pd 
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 import os
+import matplotlib.pyplot as plt
 
 class Linear__Regression():
     
     def __init__(self, file, independent, dependent,):
-        self.directory = os.getcwd() + "/" + file
-        self.X_name = independent
-        self.Y_name = dependent
+        self.directory = os.getcwd() + "/" + file # Get the file directory
+        self.X_name = independent # Variable Name
+        self.Y_name = dependent # Variable Name
+        self.continue_predicting = False
     
     def read_csv(self):
-        self.file_data = pd.read_csv(self.directory)
+        self.file_data = pd.read_csv(self.directory) # In a [[Variable1], [Variable2]] format. Regressor takes [[Variable1, Variable2], [Variable1, Variable2]] format
         #print(self.file_data.describe())
 
     def variable_reshape(self):
@@ -34,27 +35,50 @@ class Linear__Regression():
         self.test_results = self.test_results.head(200) # This is for visualization, you will visualize some of the data to see the accuracy
         #print(self.test_results)
     
-    def test_model_visualized(self):
+    def prepare_visualization(self):
         dataframe = self.test_results
         dataframe_list = dataframe.values.tolist() # Converting back to List format because MatPlotLib takes lists.
         
-        datapoints = []
-        predicted_values = []
-        actual_values = []
+        self.datapoints = []
+        self.predicted_values = []
+        self.actual_values = []
 
         for i in range(len(dataframe_list)):
-            datapoints.append(i)
-            actual_values.append(dataframe_list[i][0])
-            predicted_values.append(dataframe_list[i][1])
-        
-        plt.plot(datapoints, predicted_values, color = "red", linewidth = 1)
-        plt.plot(datapoints, actual_values, color = "red", linewidth = 1)
-        plt.fill_between(datapoints, predicted_values, actual_values, where= predicted_values > actual_values, facecolor="black")
-        plt.fill_between(datapoints, predicted_values, actual_values, where= predicted_values < actual_values, facecolor="black")
+            self.datapoints.append(i)
+            self.actual_values.append(dataframe_list[i][0])
+            self.predicted_values.append(dataframe_list[i][1])
+    
+    def visualize_error(self):
+        plt.plot(self.datapoints, self.predicted_values, color = "red", linewidth = 1)
+        plt.plot(self.datapoints, self.actual_values, color = "red", linewidth = 1)
+        plt.fill_between(self.datapoints, self.predicted_values, self.actual_values, where= self.predicted_values > self.actual_values, facecolor="black")
+        plt.fill_between(self.datapoints, self.predicted_values, self.actual_values, where= self.predicted_values < self.actual_values, facecolor="black")
         plt.xlabel("Data Points")
         plt.show()
     
-    def actual_model_visualized(self):
+    def visualize_model(self):
+        fig = plt.figure()
+        ax1 = fig.add_subplot(211)
+        ax1.scatter(self.X_test, self.Y_test, color = "blue", label = "Data Points")
+        ax1.plot(self.X_test, self.Y_pred, color = "red", label = "Model Prediction")
+        ax1.plot(0,0, color = "black", label = self.linear_regressor.score(self.X_test, self.Y_test))
+        ax1.set_xlabel(self.X_name)
+        ax1.set_ylabel(self.Y_name)
+        ax1.set_title(f"Relationship betweeb {self.X_name} and {self.Y_name}")
+        ax1.legend(loc="upper left")
+
+        ax2 = fig.add_subplot(212)
+        ax2.plot(self.datapoints, self.predicted_values, color = "red", linewidth = 1)
+        ax2.plot(self.datapoints, self.actual_values, color = "red", linewidth = 1)
+        ax2.fill_between(self.datapoints, self.predicted_values, self.actual_values, where= self.predicted_values > self.actual_values, facecolor="black")
+        ax2.fill_between(self.datapoints, self.predicted_values, self.actual_values, where= self.predicted_values < self.actual_values, facecolor="black")
+        ax2.set_xlabel("Data Points")
+        ax2.set_ylabel("Values")
+        ax2.set_title(f"Relationship between the Predicted and Actual Values")
+        plt.tight_layout(pad=0.5)
+        plt.show()
+
+        '''
         plt.scatter(self.X_test, self.Y_test, color = "blue", label = "Data Points")
         plt.plot(self.X_test, self.Y_pred, color = "red", label = "Model Prediction")
         plt.plot(0,0, color = "black", label = self.linear_regressor.score(self.X_test, self.Y_test))
@@ -63,12 +87,17 @@ class Linear__Regression():
         plt.title(f"Relationship between {self.X_name} and {self.Y_name}")
         plt.legend(loc="upper left")
         plt.show()
+        '''
     
+
+
     def execute(self):
         self.read_csv()
         self.variable_reshape()
         self.set_data()
         self.train_model()
         self.test_model()
-        self.test_model_visualized()
-        self.actual_model_visualized()
+        self.prepare_visualization()
+        #self.visualize_error()
+        self.visualize_model()
+
