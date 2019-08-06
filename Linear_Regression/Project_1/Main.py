3b48aced3c8d1310e312ce25be86e7a30ba12030
Aditya Sharma <22263756+AdityaSharma6@users.noreply.github.com>
2019-08-06 17:05:35 -0400

    Updated.

--- Linear_Regression/Project_1/Main.py
+++ Linear_Regression/Project_1/Main.py
@@ -1,8 +1,8 @@
 from Linear_Regression import Linear__Regression
 
-FILE_NAME = input("Please input the CSV File that contains your DataSet. Ex: \"Weather.csv\" ")
-X = input("What is the independent variable? ")
-Y = input("What is the dependent variable? ")
+FILE_NAME = "Weather.csv"#input("Please input the CSV File that contains your DataSet. Ex: \"Weather.csv\" ")
+X = "MinTemp"#input("What is the independent variable? ")
+Y = "MaxTemp"#input("What is the dependent variable? ")
 
 Regression = Linear__Regression(FILE_NAME, X, Y)
 Regression.execute()
