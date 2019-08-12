b5d36edf60a059f891e346ae07e572183bb1a421
Aditya Sharma <22263756+AdityaSharma6@users.noreply.github.com>
2019-08-12 13:19:34 -0400

    Modular

--- Multivariate_Linear_Regression/Project_1/Main.py
+++ Multivariate_Linear_Regression/Project_1/Main.py
@@ -1,10 +1,17 @@
 from MLR import MultivariateLinearRegression
 
-file_name = "winequality.csv"#input("Please enter the name of the CSV File that will act as the data set. Ex: \"Weather.csv\" ")
+file_name = input("Please enter the name of the CSV File that will act as the data set. Ex: \"Weather.csv\" ")
 
-#var_count = int(input("How many independent variables do you have? "))
-independent = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates','alcohol'] #[]
-dependent = "quality"#input("What is the name of the dependent variable? Please enter the exact name from the CSV. ")
+var_count = int(input("How many independent variables do you have? "))
+independent = []
+
+for i in range(len(var_count)):
+    variable = input(f"Please enter the name of variable number {i}. It should be exactly how it is written in the CSV file. Same as title.")
+    independent.append(variable)
+
+
+#independent = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates','alcohol'] #[]
+dependent = input("What is the name of the dependent variable? Please enter the exact name from the CSV. ") #"quality"
 
 MLR = MultivariateLinearRegression(file_name, independent, dependent)
 MLR.execute()
